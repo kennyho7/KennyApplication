3259e3c59b9da90f4972024073cf92adab5309b0
kennyho7 <227020198@stu.vtc.edu.hk>
2023-06-26 23:29:17 +0800

    sensor update

--- app/src/main/java/com/coventry/kennyapplication/SubmitRecordActivity.kt
+++ app/src/main/java/com/coventry/kennyapplication/SubmitRecordActivity.kt
@@ -1,7 +1,13 @@
 package com.coventry.kennyapplication
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +15,7 @@ import android.util.Log
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
@@ -27,6 +34,9 @@ class SubmitRecordActivity : AppCompatActivity() {
 
     private lateinit var viewBinding: ActivitySubmitRecordBinding
     private lateinit var cameraExecutor: ExecutorService
+    lateinit var sensorStatusTV: TextView
+    lateinit var proximitySensor: Sensor
+    lateinit var sensorManager: SensorManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -116,6 +126,25 @@ class SubmitRecordActivity : AppCompatActivity() {
             startCamera()
         } else {
             requestPermissions()
+        }
+
+        //proximity-sensor
+        sensorStatusTV = findViewById(R.id.idTVSensorStatus)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+
+        //check if the proximity sensor is null
+        if (proximitySensor == null) {
+            //display a toast if no sensor is available
+            Toast.makeText(this, "No proximity sensor found in device..", Toast.LENGTH_SHORT).show()
+            finish()
+        } else {
+            // register sensor with sensor manager
+            sensorManager.registerListener(
+                proximitySensorEventListener,
+                proximitySensor,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
         }
     }
 
@@ -215,103 +244,22 @@ class SubmitRecordActivity : AppCompatActivity() {
             }
         }
 
-    /*
-    companion object {
-        private const val TAG = "SubmitRecordActivity"
-    }
+    var proximitySensorEventListener: SensorEventListener? = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+            // method to check accuracy changed in sensor.
+        }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_submit_record)
-
-        // Enable debug-level logging for Firebase Database
-        FirebaseDatabase.getInstance().setLogLevel(Logger.Level.DEBUG)
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setDisplayShowHomeEnabled(true)
-
-        val database = FirebaseDatabase.getInstance()
-        val leaderboardRef = database.getReference("leaderboard")
-
-        val user = Firebase.auth.currentUser
-        val optionFrom = intent?.getStringExtra("option")
-
-        Log.d(TAG, "Screen: ${optionFrom}")
-
-        // Assuming you have EditText fields for username and score in your layout
-        val editTextUsername = findViewById<EditText>(R.id.edittext_username)
-        val editTextScore = findViewById<EditText>(R.id.edittext_score)
-        val buttonSubmitRecord = findViewById<Button>(R.id.button_submit_record)
-
-        buttonSubmitRecord.setOnClickListener {
-            if (NetworkUtils.isNetworkAvailable(this)) {
-                // Retrieve the values from the input fields
-                if (editTextUsername.text.isNotEmpty() || editTextScore.text.isNotEmpty()) {
-                    val username = editTextUsername.text.toString()
-                    val score = editTextScore.text.toString().toInt()
-
-                    Log.d(TAG, "user id: ${user?.uid} | username: $username | score: $score")
-
-                    // Create an instance of your leaderboard entry using the input values
-                    val leaderboardEntry = LeaderboardEntry(username, score)
-
-                    user?.let {
-                        leaderboardRef.child(it.uid).setValue(leaderboardEntry)
-                            .addOnSuccessListener {
-                                // Record submitted successfully
-                                // Handle any further actions or UI updates here
-                                Log.d(TAG, "Success")
-                                Toast.makeText(this, "Submitted your record.", Toast.LENGTH_LONG)
-                                    .show()
-                            }
-                            .addOnFailureListener { error ->
-                                // An error occurred while submitting the record
-                                // Handle the error appropriately
-                                Log.d(TAG, "Fail: $error")
-                                Toast.makeText(
-                                    this,
-                                    "Fail to submit your record.",
-                                    Toast.LENGTH_LONG
-                                )
-                                    .show()
-                            }
-                            .addOnCompleteListener { task ->
-                                if (task.isSuccessful) {
-                                    // Success
-                                } else {
-                                    // Failure
-                                    val exception = task.exception
-                                    // Log or handle the exception as needed
-                                    Log.d(TAG, "Exception: $exception")
-                                }
-                            }
-                    }
+        override fun onSensorChanged(event: SensorEvent) {
+            // check if the sensor type is proximity sensor.
+            if (event.sensor.type == Sensor.TYPE_PROXIMITY) {
+                if (event.values[0] == 0f) {
+                    sensorStatusTV.text = "Too close"
+                    sensorStatusTV.setTextColor(Color.RED)
                 } else {
-                    // Empty input, display an error message
-                    Toast.makeText(
-                        this,
-                        "Please enter a username and score before submit record",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    sensorStatusTV.text = "Can do exercise now"
+                    sensorStatusTV.setTextColor(Color.GREEN)
                 }
-            } else {
-                // No internet connection, show an error message or perform appropriate action
-                Toast.makeText(this, "No internet connection", Toast.LENGTH_LONG)
-                    .show()
             }
         }
-
     }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                onBackPressed()
-                true
-            }
-
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-     */
 }
